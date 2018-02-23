5d55cc629d81f8009ab2748b81fe7c816bc0b537
BLaurenB <blbillington1@gmail.com>
2018-02-22 23:11:34 -0700

    have google auth working

---
+++ app/controllers/ventures_controller.rb
@@ -0,0 +1,13 @@
+class VenturesController < ApplicationController
+
+
+  def index
+  #   require 'openssl'
+  #    doc = Nokogiri::HTML(open('https://trends.google.com/trends/explore?geo=US&gprop=froogle&q=bag,binoculars,laptop,ring,hat', :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
+  #
+  #    entries = doc.css('.material-icons-extended.gray')
+  #    binding.pry
+  end
+
+
+end
