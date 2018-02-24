2c885dfc6a24a408822900dca753cade5cbe3674
BLaurenB <blbillington1@gmail.com>
2018-02-24 15:19:28 -0700

    scraping works for downlaoding csv, need to check where it downloads for Heroku

--- app/controllers/ventures_controller.rb
+++ app/controllers/ventures_controller.rb
@@ -2,15 +2,20 @@ class VenturesController < ApplicationController
 
 
   def index
-
-    # agent = Mechanize.new
-    # page = agent.get('https://trends.google.com/trends/explore?geo=US&gprop=froogle&q=bag,binoculars,laptop,ring,hat')
-    # pp page
+    # https://trends.google.com/trends/explore?geo=US&gprop=froogle&q=bag,binoculars,laptop,ring,hat
 
 
     browser = Watir::Browser.new
-    browser.goto('https://trends.google.com/trends/explore?geo=US&gprop=froogle&q=bag,binoculars,laptop,ring,hat'
-    puts browser.title
+    browser.goto('https://trends.google.com/trends/explore?geo=US&gprop=froogle&q=bag,map,laptop,ring,hat')
+    # @watir_result =  browser.element(css: 'article header h2')
+    # binding.pry
+    browser.button(text: 'file_download').click
+
+    # browser.button(:xpath => "//button[@onclick='wizardAction(\'start\', \'Starte Upgrade\')']").click
+    # element.class_name
+    # element.click
+    # browser.wait_until { browser.h1.text != 'Main Page' }
+
     browser.close
 
   end
