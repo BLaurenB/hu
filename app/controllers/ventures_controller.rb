c360313afe40e0052bee02c902f3fcb21846cfcf
BLaurenB <blbillington1@gmail.com>
2018-02-24 19:50:50 -0700

    google js working in view

--- app/controllers/ventures_controller.rb
+++ app/controllers/ventures_controller.rb
@@ -2,16 +2,29 @@ class VenturesController < ApplicationController
 
 
   def index
-    # https://trends.google.com/trends/explore?geo=US&gprop=froogle&q=bag,binoculars,laptop,ring,hat
+
+    # -------------------------------------------
+    # Google Trends - Shopping and Search
+    # -------------------------------------------
 
 
-    browser = Watir::Browser.new
-    browser.goto('https://trends.google.com/trends/explore?geo=US&gprop=froogle&q=bag,map,laptop,ring,hat')
-    # @watir_result =  browser.element(css: 'article header h2')
-    # binding.pry
-    browser.button(text: 'file_download').click
-    browser.close
+    #get search terms from Terms table
+    # browser = Watir::Browser.new
+    # browser.goto('https://trends.google.com/trends/explore?geo=US&gprop=froogle&q=bag,map,laptop,ring,hat')
+    # browser.button(text: 'file_download').click
+    #
+    # browser.goto('https://trends.google.com/trends/explore?geo=US&q=news,backpack,commuter,bus,laptop')
+    # browser.button(text: 'file_download').click
+    #
+    # browser.close
     #MAKE SURE TO PRVIDE A LINK TO THE GOOGLE PAGE!!!!
+
+
+
+    # -------------------------------------------
+    # Census Trends  - Overall US, Industry specific
+    # -------------------------------------------
+
 
   end
 
