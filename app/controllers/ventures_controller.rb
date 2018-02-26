6590bac8da498d590f47adb6b6ea33c81151407d
BLaurenB <blbillington1@gmail.com>
2018-02-26 00:32:11 -0700

    styling changes added for pushing to heroku

--- app/controllers/ventures_controller.rb
+++ app/controllers/ventures_controller.rb
@@ -7,23 +7,6 @@ class VenturesController < ApplicationController
 
   def show
 
-
-    #SEARCHING
-      # browser = Watir::Browser.new
-      # browser.goto("https://trends.google.com/trends/explore?geo=US&q=news,#{@term.word_1},#{@term.word_2},#{@term.word_3},#{@term.word_4}")
-      # browser.button(text: 'file_download').click
-      # browser.close
-      #
-      #   file_path_2 = "/Users/laurenbillington/Downloads/multiTimeline.csv"
-      #   csv_text_2 = File.open(File.expand_path(__FILE__, file_path_2)).read
-      #   # csv_text_2 = File.read(file_path_2)
-      #   csv = CSV.parse(csv_text_2, :headers => true, :encoding => 'ISO-8859-1')
-      #   csv.each do |row|
-      #     GoogleSearching.create(row.to_hash)
-      #   end
-
-
-
     # -------------------------------------------
     # Census Trends  - Overall US, Industry specific
     # -------------------------------------------
@@ -32,7 +15,6 @@ class VenturesController < ApplicationController
     @term = @venture.terms.first # ".first" will change based on whether the user has updated search terms
     industry = @venture.category_id
     @hu_insight = HuInsights.insight_maker(@term) #industry
-    binding.pry
 
   end
 
