23fdf9c82b4aaa22695d176fcbc5212bd3a53544
BLaurenB <blbillington1@gmail.com>
2018-02-28 14:44:55 -0700

    finished service tests, moving on to model tests

--- spec/models/authentication_spec.rb
+++ spec/models/authentication_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Authentication, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "Authentication" do
+  it "can be created with valid oath params" do
+    user = User.create
+    new_auth = Authentication.create(provider: "google_oauth2", uid: "123456789", name: "Lauren", oauth_token: "niftytokenhere", oauth_expires_at: "2018-02-23 06:29:02", user_id: user.id)
+
+    expect(new_auth).to be_an_instance_of(Authentication)
+  end
+
 end
---
+++ spec/models/google_shop_weekly_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe " Google Shop Weekly" do
+
+    it "creates new GoogleShopWeekly instances from a hash" do
+      row_hash =
+        {"Week"=>"2017-02-26", "news: (United States)"=>"76", "backpack: (United States)"=>"2", "commuter: (United States)"=>"1", "bus: (United States)"=>"12", "laptop: (United States)"=>"5"}
+
+      week_stats = GoogleShopWeekly.new(row_hash)
+      expect(week_stats.standard).to eq(76)
+      expect(week_stats.word_1).to eq(2)
+      expect(week_stats.word_2).to eq(1)
+      expect(week_stats.word_3).to eq(12)
+      expect(week_stats.word_4).to eq(5)
+
+    end
+
+end
